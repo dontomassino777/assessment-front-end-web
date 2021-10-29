6e93371c19e20ac9d5cf8efbeb91d7188c43544e
Tomas Fisher <dontomassino777@gmail.com>
2021-10-29 11:31:41 -0600

    part 5 completed

--- about.js
+++ about.js
@@ -5,10 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("This form has been submitted");
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let duckImg = document.querySelector("#duck-img")
+
+
+function payCompliment() {
+	alert("You are ducking amazing!!!")
+}
+
+duckImg.addEventListener("mouseover", payCompliment)
---
+++ profile.js
@@ -0,0 +1,18 @@
+const favColBtn = document.querySelector("#color")
+const favPlBtn = document.querySelector("#place")
+const favRitBtn = document.querySelector("#ritual")
+
+
+favColBtn.addEventListener("click",() => {
+    alert("Seafoam green!");
+})
+
+favPlBtn.addEventListener("click", () => {
+    alert("Wherever I am at that moment")
+})
+
+favRitBtn.addEventListener("click", () => {
+    alert("Practicing Transcendental Meditation 20 minutes a day")
+})
+
+
